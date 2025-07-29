c1ef08cf5ba8dabf31fd39e45a362fc81b9c6a29
Vinniel <155176879+Vinniel5403@users.noreply.github.com>
2025-07-29 23:04:55 +0700

    Hero

--- src/app/Landing/Components/HeroSection.tsx
+++ src/app/Landing/Components/HeroSection.tsx
@@ -1,9 +1,47 @@
 import React from "react";
-
+import Image from "next/image";
 function HeroSection() {
   return (
     <div className="h-screen flex flex-col justify-center items-center">
-      Hero Section
+      <div className="flex">
+        <div>
+          <h1 className="text-red text-4xl font-bold">
+            ไอเดียและเรื่องราว
+            <br />
+            อันควรค่าแก่การเผยแพร่
+          </h1>
+          <Image
+            width={400}
+            height={400}
+            src="/Landing/Arrow.png"
+            alt="TedXYouth Bangkok"
+            className="w-[80%]"
+          />
+          <p className="text-red text-left">
+            เพื่อสร้างแรงบันดาลใจดีๆให้กับทุกคน
+            <br /> เพื่อนำกลับไปสร้างการเปลี่ยนแปลงทางสังคม
+          </p>
+          <div className="flex gap-4 mt-12">
+            <button className="bg-red px-8 py-4 rounded-full text-white font-medium cursor-pointer">Join Event</button>
+            <button className="flex items-center gap-2 bg-white rounded-full text-black font-medium cursor-pointer">
+            <div className="flex items-center gap-2">
+                <Image width={80} height={80} src="/Landing/Play.svg" alt="Play Icon" className="min-h-10 h-[100%] aspect-square w-auto" />
+                <p>ชม Talk ที่ผ่านมา</p>
+            </div>
+               </button>
+          </div>
+        </div>
+        <div>
+          {" "}
+          <Image
+            width={600}
+            height={600}
+            src="/Landing/Landing.png"
+            alt="TedXYouth Bangkok"
+            className="w-[80%]"
+          />
+        </div>
+      </div>
     </div>
   );
 }
