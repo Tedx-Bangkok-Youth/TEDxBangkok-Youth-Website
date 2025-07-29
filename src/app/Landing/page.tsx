5862cc39fb12df48ea36005fb4f8c3222c396e89
Vinniel <155176879+Vinniel5403@users.noreply.github.com>
2025-07-29 22:32:02 +0700

    Feat : Landing Page

---
+++ src/app/Landing/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Landing() {
+  return (
+    <div className="flex flex-col justify-center items-center gap-16">
+      <div className="h-screen flex flex-col justify-center items-center">Main Landing page component</div>
+      <div className="h-screen flex flex-col justify-center items-center">Slider</div>
+      <div className="h-screen flex flex-col justify-center items-center">Make it you</div>
+          <div className="h-screen flex flex-col justify-center items-center">Timeline</div>
+                 <div className="h-screen flex flex-col justify-center items-center">Map</div>
+        <div className="h-screen flex flex-col justify-center items-center">Story</div>
+    </div>
+  );
+}
+
+export default Landing;
