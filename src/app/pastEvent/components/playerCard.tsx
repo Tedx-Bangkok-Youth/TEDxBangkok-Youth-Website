ea453509cfa1da087e56db8d933ad5804caae1cb
ptteerat <teerat5826@gmail.com>
2025-08-02 02:54:15 +0700

    pastEvent : speakerSideTesting

--- src/app/pastEvent/components/playerCard.tsx
+++ src/app/pastEvent/components/playerCard.tsx
@@ -7,11 +7,11 @@ interface PlayerCardProps {
 
 const PlayerCard: React.FC<PlayerCardProps> = ({ imageUrl, text }) => {
   return (
-    <div style={{borderRadius: 16, padding: 16, textAlign: "center", width: 220 }}>
+    <div style={{borderRadius: 16, padding: 8, textAlign: "center", width: 220 }}>
       <img
         src={imageUrl}
         alt="Player"
-        style={{ width: 245, height: 171, borderRadius: 16, objectFit: "cover", marginBottom: 16 }}
+        style={{ width: 245, height: 171, borderRadius: 8, objectFit: "cover", marginBottom: 16 }}
       />
       <div style={{ textAlign: "left", fontSize: 15, color: "#222" }}>{text}</div>
     </div>
